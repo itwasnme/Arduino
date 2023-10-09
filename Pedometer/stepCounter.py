12b11a261a671b309b84ee88d5539ff743509193
Nicolas Meneses <nm3bkj@virginia.edu>
2023-10-09 17:46:29 -0400

    Public Version

--- Pedometer/stepCounter.py
+++ Pedometer/stepCounter.py
@@ -16,28 +16,3 @@ for i in range(len(YAcceleration)):
             steps += 1
             ascending = False;  
 print(steps)
-
-# max_value = 0
-# min_value = 1000
-# counter = 0
-
-# for i in range(len(Time)):
-#     if AbsAcceleration[i] > max_value:
-#         max_value = AbsAcceleration[i]
-#     elif AbsAcceleration[i] < min_value:
-#         min_value = AbsAcceleration[i]
-#     counter += 1
-#     if counter == 50:
-#         # print(str(max_value) + " " + str(min_value))
-#         if max_value-min_value >= 4:
-#             steps += 1
-#         counter = 0
-#         max_value = 0
-#         min_value = 1000
-
-# if counter != 0: #accounts for last time frame which might not be 1 sec
-#     if max_value-min_value >= 4:
-#         # print(str(max_value) + " " + str(min_value))
-#         steps += 1
-
-# print(steps)
